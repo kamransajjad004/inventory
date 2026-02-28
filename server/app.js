a32f8d967694a596b090c617f81024dc4c906632
mohdafnaan <mohdafnaan833@gmail.com>
2026-03-01 01:02:37 +0530

    server setup done

---
+++ server/app.js
@@ -0,0 +1,22 @@
+import express from "express"
+import dotenv from "dotenv"
+dotenv.config()
+
+
+const app = express()
+app.use(express.json())
+
+const port = process.env.PORT
+
+app.get("/",(req,res)=>{
+    try {
+        res.status(200).json({msg : "sever is running"})
+    } catch (error) {
+        console.log(error)
+        res.status(500).json(error)
+    }
+})
+
+app.listen(port,()=>{
+    console.log(`server is running at port 5555`)
+})
